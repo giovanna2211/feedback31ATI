5db2edd7ff6e66219ea885e0092a03f4af820701
giscalzone <giscalzone@gmail.com>
2025-03-31 22:35:14 +0000

    adaptação de responsividade para o desktop e fim do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export function App(){
   return(
 
     submited == false ? (
-      <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass">
+      <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass max-w-103">
       <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
         <img src={IconStar} alt="Icon star" />
       </div>
@@ -30,17 +30,17 @@ export function App(){
 
       <p className="text-light-grey text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
       <div className="flex justify-between mb-6">
-        <input type="button" value={1} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleMudarNotaAvaliacao(1)}/>
-        <input type="button" value={2} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleMudarNotaAvaliacao(2)}/>
-        <input type="button" value={3} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleMudarNotaAvaliacao(3)}/>
-        <input type="button" value={4} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleMudarNotaAvaliacao(4)}/>
-        <input type="button" value={5} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleMudarNotaAvaliacao(5)}/>
+        <input type="button" value={1} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer sm:w-13 sm:h-13" onClick={() => handleMudarNotaAvaliacao(1)}/>
+        <input type="button" value={2} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer sm:w-13 sm:h-13" onClick={() => handleMudarNotaAvaliacao(2)}/>
+        <input type="button" value={3} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer sm:w-13 sm:h-13" onClick={() => handleMudarNotaAvaliacao(3)}/>
+        <input type="button" value={4} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer sm:w-13 sm:h-13" onClick={() => handleMudarNotaAvaliacao(4)}/>
+        <input type="button" value={5} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white hover:cursor-pointer sm:w-13 sm:h-13" onClick={() => handleMudarNotaAvaliacao(5)}/>
       </div>
 
       <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold hover:bg-white hover:text-orange hover:cursor-pointer" onClick={handleSubmit}>Submit</button>
     </div>
     ) : (
-      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
         <img className="mx-auto mb-6" src= {IllustrationThankYou} alt="Illustration Thank You" />
 
         <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You selected {notaAvaliacao} out of 5</p>
