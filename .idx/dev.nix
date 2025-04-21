48eab852bea461db80ce95eb8d61e4abe7d78bbb
giscalzone <giscalzone@gmail.com>
2025-04-21 23:49:41 +0000

    ajustes de responsividade dos botões de avaliação no primeiro card

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,7 +4,7 @@
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [
+''  packages = [
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
